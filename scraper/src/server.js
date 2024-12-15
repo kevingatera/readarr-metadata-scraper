15da98bbeda25aaaf0781c406d6334b2e6014074
Kevin Gatera <kevin@deployitwith.me>
2024-12-15 18:31:17 -0500

    A bit more formatting

--- scraper/src/server.js
+++ scraper/src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import fs from "fs";
 import http from "http";
 import https from "https";
-import {getBook} from './bookscraper.js'
+import { getBook } from './bookscraper.js'
 import getAuthor from "./authorscraper.js";
 import { getFromCache, saveToCache } from './cache.js';
 
@@ -19,37 +19,37 @@ function generateCacheKey(fetchFn, args) {
 const MAX_RETRIES = 5;
 const BASE_DELAY = 1000; // 1 second
 
-const privateKey  = fs.readFileSync('./certs/bookinfo-club.key', 'utf8');
+const privateKey = fs.readFileSync('./certs/bookinfo-club.key', 'utf8');
 const certificate = fs.readFileSync('./certs/bookinfo-club.crt', 'utf8');
 
-const credentials = {key: privateKey, cert: certificate};
+const credentials = { key: privateKey, cert: certificate };
 
 const app = express()
 app.use(express.json())
 
-app.get('/v1/author/:id', async (req, res)=>{
+app.get('/v1/author/:id', async (req, res) => {
     const id = req.params.id
-    console.log('requesting author id /v1/author',id)
+    console.log('requesting author id /v1/author', id)
     const goodreadsUrl = `https://www.goodreads.com/author/show/${id}`
-    const authorInfo= await getAuthor(id,goodreadsUrl)
+    const authorInfo = await getAuthor(id, goodreadsUrl)
     console.log(authorInfo)
     res.status(200);
-    res.send({...authorInfo,Works:[]})
+    res.send({ ...authorInfo, Works: [] })
 });
-app.get('/v1/work/:id', async (req, res)=>{
+app.get('/v1/work/:id', async (req, res) => {
     const id = req.params.id
     console.log('getting work /v1/work', id)
-    const {work,author} = await getBook(id)
-    const authorInfo= await getAuthor(author[0].id,author[0].url)
-    const response= {
-        ForeignId:work.ForeignId,
+    const { work, author } = await getBook(id)
+    const authorInfo = await getAuthor(author[0].id, author[0].url)
+    const response = {
+        ForeignId: work.ForeignId,
         Title: work.Title,
         Url: work.Url,
-        Genres:["horror"],
-        RelatedWorks:[],
-        Books:[work],
-        Series:[],
-        Authors:[authorInfo]
+        Genres: ["horror"],
+        RelatedWorks: [],
+        Books: [work],
+        Series: [],
+        Authors: [authorInfo]
     }
     console.log(response)
     res.send(response)
@@ -72,7 +72,7 @@ async function fetchWithRetry(fetchFn, ...args) {
             return result;
         } catch (error) {
             if (attempt === MAX_RETRIES) throw error;
-            
+
             const delay = BASE_DELAY * Math.pow(2, attempt - 1);
             console.log(`Attempt ${attempt} for ${fetchFn.name} failed. Retrying in ${delay}ms`);
             await new Promise(resolve => setTimeout(resolve, delay));
@@ -90,13 +90,13 @@ app.post('*', async (req, res) => {
             console.log('getting', id);
             const bookResult = await fetchWithRetry(getBook, id);
             console.log('retrieved book:', bookResult);
-            
+
             if (!(bookResult.author[0].id in authors)) {
                 const authorResult = await fetchWithRetry(getAuthor, bookResult.author[0].id, bookResult.author[0].url);
                 authors[bookResult.author[0].id] = authorResult;
                 console.log('retrieved author:', authorResult);
             }
-            
+
             books.push(bookResult);
         } catch (error) {
             console.error(`Completely failed to fetch book ${id}:`, error.message);
